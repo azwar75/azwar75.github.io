f3e483df27c8e019a16a106838f325d70b486c74
azwar75 <50655271+azwar75@users.noreply.github.com>
2020-08-20 22:01:25 +0700

    seputarbola

--- js/regis.js
+++ js/regis.js
@@ -2,7 +2,7 @@
     if ("serviceWorker" in navigator) {
         window.addEventListener("load", function() {
           navigator.serviceWorker
-            .register("../service-worker1.js")
+            .register("../service-worker.js")
             .then(function() {
               console.log("Pendaftaran ServiceWorker berhasil");
             })
